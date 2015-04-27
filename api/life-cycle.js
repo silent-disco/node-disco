1091cd5ac16faf0d94eb481062ede353329fdbbe
Ricardo Matias <ricardo.matias0@gmail.com>
2015-04-27 21:49:03 +0200

    feat(build): add gulp to development

--- api/life-cycle.js
+++ api/life-cycle.js
@@ -3,17 +3,22 @@ var co = require('co');
 
 function LifeCycle(events) {
 
-  function shutdown() {
+  function cleanup() {
     console.log('\033[2K\033[4D[node-disco] server shutting down');
 
-    co(function*() {
+    return co(function*() {
       yield events.emit('shutdown');
 
       console.log('[node-disco] over and out.');
+    });
+  }
 
+  function shutdown() {
+    return cleanup().then(function() {
       process.exit(0);
     });
   }
+
 
   // start reading from stdin so we don't exit.
   process.stdin.resume();
@@ -22,6 +27,14 @@ function LifeCycle(events) {
   process.on('SIGINT', shutdown);
   process.on('SIGTERM', shutdown);
   process.on('SIGHUP', shutdown);
+
+  // nodemon signal integration, cleanly shutting down
+  // the app after cleanup
+  process.once('SIGUSR2', function() {
+    return cleanup().then(function() {
+      process.kill(process.pid, 'SIGUSR2');
+    });
+  });
 }
 
 
---
+++ gulpfile.js
@@ -0,0 +1,154 @@
+'use strict';
+
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    size = require('gulp-size'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    stylish = require('jshint-stylish'),
+    less = require('gulp-less'),
+    watchify = require('watchify'),
+    browserify = require('browserify'),
+    del = require('del'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    gutil = require('gulp-util'),
+    livereload = require('gulp-livereload'),
+    nodemon = require('gulp-nodemon'),
+    assign = require('lodash/object/assign');
+
+
+var dest = 'public';
+
+// add custom browserify options here
+var browserifyOptions = {
+  entries: ['./client/client.js'],
+  debug: true
+};
+
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+function bundle(options) {
+
+  var bundler;
+
+  var bundleOptions;
+
+  function build() {
+    return bundler
+             .bundle()
+               .pipe(plumber())
+               .pipe(source('client.js'))
+               .pipe(buffer())
+               .pipe(gulp.dest(dest))
+               .pipe(livereload())
+               .pipe(rename({ extname: '.min.js' }))
+               .pipe(uglify())
+               .pipe(gulp.dest(dest))
+               .pipe(livereload());
+  }
+
+  if (options && options.watch) {
+
+    bundleOptions = assign({}, watchify.args, browserifyOptions);
+
+    bundler = watchify(browserify(bundleOptions));
+
+    bundler.on('update', build);
+    bundler.on('log', gutil.log);
+  } else {
+    bundler = browserify(browserifyOptions);
+  }
+
+  bundler.build = build;
+
+  return bundler;
+}
+
+function copy(src, dir) {
+  return gulp.src(src)
+             .pipe(gulp.dest(dest + (dir ? '/' + dir : '')))
+             .pipe(livereload());
+}
+
+function lint(src) {
+  return gulp.src(src)
+               .pipe(plumber())
+               .pipe(jshint('.jshintrc'))
+               .pipe(jshint.reporter(stylish));
+}
+
+gulp.task('client:build:watch', ['client:lint'], function() {
+  return bundle({ watch: true }).build();
+});
+
+gulp.task('client:build', [ 'client:lint' ], function() {
+  return bundle().build();
+});
+
+gulp.task('client:clean', function(cb) {
+  return del('public/**/*', cb);
+});
+
+gulp.task('client:less', function() {
+  return gulp.src('client/style.less')
+             .pipe(plumber())
+             .pipe(less())
+             .pipe(autoprefixer())
+             .pipe(gulp.dest(dest))
+             .pipe(livereload());
+});
+
+gulp.task('client:lint', function() {
+  return lint('client/**/*.js');
+});
+
+gulp.task('client:copy', [ 'client:copy:html', 'client:copy:font' ]);
+
+gulp.task('client:copy:html', function() {
+  return copy('client/index.html');
+});
+
+gulp.task('client:copy:font', function() {
+  return copy('client/font/**', 'font');
+});
+
+gulp.task('client:size', function() {
+  return gulp.src(dest + '/**').pipe(size({ showFiles: true }));
+});
+
+gulp.task('server', ['nodemon'], function() {
+  livereload.listen();
+
+  gulp.watch(['public/**/*'], [ 'client:size' ]);
+
+  gulp.watch(['client/**/*.less'], ['client:less']);
+  gulp.watch(['client/index.html'], [ 'client:copy' ]);
+});
+
+gulp.task('nodemon', function(cb) {
+  var called = false;
+  return nodemon({
+    script: 'index.js',
+    watch: ['api/**/*.js'],
+    ignore: ['node_modules', 'public/bower_components']
+  })
+  .on('start', function onStart() {
+    if (!called) {
+      cb();
+    }
+    called = true;
+    console.log('Starting...');
+  })
+  .on('change', [ 'server:lint' ]);
+});
+
+
+gulp.task('default', ['build']);
+
+gulp.task('build', [ 'client:clean', 'client:copy', 'client:less', 'client:build' ]);
+
+gulp.task('serve', [ 'client:copy', 'client:less', 'client:build:watch', 'server' ]);
